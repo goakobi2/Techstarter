8b29d23251f2e702bad5145d4893b4075b1880fd
goakobi2 <goakobi@gmail.com>
2024-10-30 14:02:50 +0100

    y

--- NodeJS/NodeJS - Hausaufgaben/30-10-24/index.js
+++ NodeJS/NodeJS - Hausaufgaben/30-10-24/index.js
@@ -1,23 +1,46 @@
+// Pakete importieren
+import inspirationalQuotes from 'inspirational-quotes';
+import inquirer from 'inquirer';
 import boxen from 'boxen';
 import chalk from 'chalk';
-import inquirer from 'inquirer';
-import inspirationalQuotes from 'inspirational-quotes';
 
-// Zitat
-function showQuote(){
-    const quote = inspirationalQuotes.getQuote();
-    const quoteBox = boxen( quote.text , {padding: 1, margin: 1, borderStyle: 'double'});
-    console.log(quoteBox);
+// Funktion zur Anzeige eines inspirierenden Zitats
+function showInspirationalQuote() {
+    const quote = inspirationalQuotes.getQuote(); // Fülle dies aus!
+    const quoteBox = boxen(chalk.green(quote.text), {
+        padding: 1,
+        margin: 1,
+        borderStyle: 'double'
+    });
+    console.log(quoteBox); // Zitat in einer Box anzeigen
 }
 
-// Benutzerinteraktion
+// Funktion für die Benutzerinteraktion
 async function askForNewQuote() {
-    
+    const answers = await inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'getQuote',
+            message: 'Möchtest du ein inspirierendes Zitat sehen?',
+            default: true,
+        },
+    ]);
+
+    if (answers.getQuote) {
+        showInspirationalQuote();
+        askForNewQuote(); // Fragt erneut, ob ein weiteres Zitat angezeigt werden soll
+    } else {
+        console.log(chalk.yellow('Danke, dass du das Programm genutzt hast!'));
+        process.exit(0); // Programm beenden
+    }
 }
 
 // Hauptfunktion
 async function main() {
-    showQuote();
+    console.log(chalk.blue('Willkommen zu deinem inspirierenden Zitat-Tool!'));
+    console.log('Jetzt hast du die Möglichkeit, dein inspirierendes Zitat-Tool zu erstellen und gleichzeitig etwas über die verwendeten Module zu lernen. Viel Spaß beim Programmieren! Wenn du Fragen hast oder Hilfe benötigst, zögere nicht, nachzufragen!');
+    await askForNewQuote(); // Starte die Benutzerinteraktion
 }
 
+// Hauptfunktion aufrufen
 main();
