2523164fbbce71bbf2d2cee3b7823712c60f7d7c
goakobi2 <goakobi@gmail.com>
2024-10-30 14:22:08 +0100

    Hausaufgabe

--- NodeJS/NodeJS - Hausaufgaben/30-10-24/index.js
+++ NodeJS/NodeJS - Hausaufgaben/30-10-24/index.js
@@ -1,34 +1,47 @@
 // Pakete importieren
-import inspirationalQuotes from 'inspirational-quotes';
+import catFacts from 'cat-facts';
 import inquirer from 'inquirer';
 import boxen from 'boxen';
 import chalk from 'chalk';
 
-// Funktion zur Anzeige eines inspirierenden Zitats
-function showInspirationalQuote() {
-    const quote = inspirationalQuotes.getQuote(); // Fülle dies aus!
-    const quoteBox = boxen(chalk.green(quote.text), {
+// Funktion zur Anzeige eines Katzenfakts
+function showCatFact() {
+    const fact = catFacts.random(); // Zufälliger Katzenfakt
+    console.log("Debug: Fact fetched - ", fact); // Debugging-Statement
+    const factBox = boxen(chalk.green(fact), {
         padding: 1,
         margin: 1,
         borderStyle: 'double'
     });
-    console.log(quoteBox); // Zitat in einer Box anzeigen
+    console.log(factBox); // Fakt in einer Box anzeigen
+}
+
+// Funktion zur Abfrage des Benutzernamens
+async function askForUsername() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Wie ist dein Name?',
+        },
+    ]);
+    return answers.username; // Benutzername zurückgeben
 }
 
 // Funktion für die Benutzerinteraktion
-async function askForNewQuote() {
+async function askForNewCatFact() {
     const answers = await inquirer.prompt([
         {
             type: 'confirm',
-            name: 'getQuote',
-            message: 'Möchtest du ein inspirierendes Zitat sehen?',
+            name: 'getFact',
+            message: 'Möchtest du einen zufälligen Katzenfakt sehen?',
             default: true,
         },
     ]);
 
-    if (answers.getQuote) {
-        showInspirationalQuote();
-        askForNewQuote(); // Fragt erneut, ob ein weiteres Zitat angezeigt werden soll
+    if (answers.getFact) {
+        showCatFact();
+        console.log(chalk.yellow('Danke, dass du das Programm genutzt hast!'));
     } else {
         console.log(chalk.yellow('Danke, dass du das Programm genutzt hast!'));
         process.exit(0); // Programm beenden
@@ -37,9 +50,10 @@ async function askForNewQuote() {
 
 // Hauptfunktion
 async function main() {
-    console.log(chalk.blue('Willkommen zu deinem inspirierenden Zitat-Tool!'));
-    console.log('Jetzt hast du die Möglichkeit, dein inspirierendes Zitat-Tool zu erstellen und gleichzeitig etwas über die verwendeten Module zu lernen. Viel Spaß beim Programmieren! Wenn du Fragen hast oder Hilfe benötigst, zögere nicht, nachzufragen!');
-    await askForNewQuote(); // Starte die Benutzerinteraktion
+    const username = await askForUsername(); // Benutzername abfragen
+    console.log(chalk.blue(`Willkommen, ${username}, zu deinem Katzenfakt-Tool!`));
+    console.log('Jetzt hast du die Möglichkeit, dein Katzenfakt-Tool zu nutzen und gleichzeitig etwas über die verwendeten Module zu lernen. Viel Spaß beim Programmieren! Wenn du Fragen hast oder Hilfe benötigst, zögere nicht, nachzufragen!');
+    await askForNewCatFact(); // Starte die Benutzerinteraktion
 }
 
 // Hauptfunktion aufrufen
